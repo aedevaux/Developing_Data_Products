84e680b0a0ab9c8d437ac3ab4c426bab517c72e0
Anne deVaux <aedevaux@bellsouth.net>
2014-06-19 17:53:51 -0400

    Add files for peer review project

---
+++ FuelEconomy/server.R
@@ -0,0 +1,50 @@
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+          "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+library(AppliedPredictiveModeling)
+## Import FuelEconomy data from the APM model
+data(FuelEconmy)
+
+## Change the factor variables in the three data sets to numeric
+
+cars2012$Transmission <- as.numeric(cars2012$Transmission)
+cars2012$AirAspirationMethod <- as.numeric(cars2012$AirAspirationMethod)
+cars2012$DriveDesc <- as.numeric(cars2012$DriveDesc)
+cars2012$CarlineClassDesc <- as.numeric(cars2012$CarlineClassDesc)
+
+cars2011$Transmission <- as.numeric(cars2011$Transmission)
+cars2011$AirAspirationMethod <- as.numeric(cars2011$AirAspirationMethod)
+cars2011$DriveDesc <- as.numeric(cars2011$DriveDesc)
+cars2011$CarlineClassDesc <- as.numeric(cars2011$CarlineClassDesc)
+
+cars2010$Transmission <- as.numeric(cars2010$Transmission)
+cars2010$AirAspirationMethod <- as.numeric(cars2010$AirAspirationMethod)
+cars2010$DriveDesc <- as.numeric(cars2010$DriveDesc)
+cars2010$CarlineClassDesc <- as.numeric(cars2010$CarlineClassDesc)
+
+shinyServer(function(input, output, session) {
+  
+  #Choose dataset
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "cars2010" = cars2010,
+           "cars2011" = cars2011,
+           "cars2012" = cars2012)
+  })
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    datasetInput()[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })
+  
+})
---
+++ FuelEconomy/ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel('k-means clustering of Fuel Economy Dataset'),
+  
+  sidebarPanel(
+    selectInput('dataset', 'Choose Data Set', choices = c('cars2010', 'cars2011', 'cars2012')),
+    selectInput('xcol', 'X Variable', names(cars2012)),
+    selectInput('ycol', 'Y Variable', names(cars2012),
+                selected=names(cars2012)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 9),
+    p("This Shiny App allows you to explore the three data sets 
+    in the FuelEconomy dataset in the Applied Predictive Modeling Package in R.")),
+  
+  mainPanel(
+    plotOutput('plot1'),
+    p("The", a(href="http://fueleconomy.gov", "http://fueleconomy.gov"),"website, run by the U.S. Department of Energy's Office of Energy Efficiency and Renewable Energy 
+    and the U.S. Environmental Protection Agency, lists different estimates of fuel economy for passenger cars and trucks. 
+    For each vehicle, various characteristics are recorded such as the engine displacement or number of cylinders. 
+    Along with these values, laboratory measurements are made for the city and highway miles per gallon (MPG) of the car.")  
+  )
+))
